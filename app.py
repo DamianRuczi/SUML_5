eb40ef143a022a30d4931661e035081d25c0209e
DamianRuczi <s27499@pjwstk.edu.pl>
2026-05-16 13:42:27 +0200

    skeleton of the architecture - end of the class

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 # Miejsce hostowania aplikacji
+
+import uvicorn
+from fastapi import FastAPI, HTTPExecution, Form
+from pathlib import Path
+from typing import Annotated
+from models.point import Point
+from libs.model import train_linear_regression_model, predict_target_value
+
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+MODEL_DIR = Path(BASE_DIR).joinpath("ml_models")
+DATA_DIR = Path(BASE_DIR).joinpath("data")
+
+
+app = FastAPI()
+
+@app.get("/", tags=["intro"])
+def index():
+    return {"message": "Linear Regression"}
+
+
+@app.post("/model/train")
+async def train_model():
+    return {"message": "Should train model"}
--- model/point.py
+++ model/point.py
@@ -1,1 +1,8 @@
 # Tutaj sobie zdefiniujemy kolejne elementy naszego modelu, czyli pomocnicze klase które będą w tym modelu reprezentowane.
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Point(BaseModel):
+    
